3974348f6f21fa63a7a5f15468445cd3867641ff
patpaw111 <mfaqih3562@gmail.com>
2026-05-26 23:24:20 +0700

    belajar array

--- rust-dasar/src/main.rs
+++ rust-dasar/src/main.rs
@@ -70,7 +70,50 @@ fn boolean_operator() {
     let a = true;
     let b = false;
 
-    println!("{}", a && b); // and operator syara ke2 true menghasilkan true selain itu false
+    println!("{}", a && b); // and operator syarat ke2 true menghasilkan true selain itu false
     println!("{}", a || b); // or operator salah satu true maka hasilnya true, gk ada  maka false
     println!("{}", !b); // not operator jika tidak sama / jika false maka true / jika true maka false
+}
+
+// mengenal tipe data compound yg isinya bisa banyak tipe data lainnya
+#[test]
+fn tuple() {
+    let data: (i32, f32, bool) = (332, 3.14, true); // tuple bagusnya digunakan untuk tipe data yg berbeda
+    println!("{:?}", data);
+
+    // cara mengakses data dari tuple  dengan cara .nomor-indeks dari data tersebut, urutan indeks dimulai dari 0
+    println!("{:?}", data.0);
+
+    // ini dinamakan destructuring tuple dmna data dari tuple di atas di bongkar dan di masukan ke dalam variable
+    let (a, b, _) = data; // data yg tidak gunakan bisa di suprres dengan _ agar tidak warning
+    println!("{} {}", a, b);
+
+    // contoh mutable tuple
+    let mut jumlah_siswa_tiap_kelas: (i32, i32, bool)  = (33, 29, true);
+
+    // dengan tuple yg mutable bisa di ubah nilai dri setiap indexnya
+    jumlah_siswa_tiap_kelas.0 = 30;
+    println!("{:?}", jumlah_siswa_tiap_kelas);
+}
+
+// contoh unit = tuple yg kosong
+fn _unit() {
+    println!("Hello");
+}
+
+#[test]
+fn test_unit() {
+    let result = _unit(); // bisa diliat bahwa hasil dri fn unit itu tuple kosong saya juga masih bingung
+    println!("{:?}", result);
+}
+
+#[test]
+fn array() {
+    // membuat array dengann implisit/tidak di sebutkan jenis dan jumlah datannya
+    let array1 = [1, 2, 3, 4];
+    println!("{:?}", array1);
+
+    // membuat array dengan cara explisit/tipe data dan jumlahnya disebutkan
+    let array2: [i8; 6] = [1, 2, 3, 4, 5, 6];
+    println!("{:?}", array2);
 }
